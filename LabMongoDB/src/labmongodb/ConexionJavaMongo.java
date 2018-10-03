e461be963f891f8c1c61099d40cbbf5e446996d8
Thomas Mejias <tmejias2302@gmail.com>
2018-10-03 09:58:43 -0600

    Initial commit.

--- LabMongoDB/src/labmongodb/ConexionJavaMongo.java
+++ LabMongoDB/src/labmongodb/ConexionJavaMongo.java
@@ -1,6 +1,25 @@
 package labmongodb;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import java.util.List;
+
 
 public class ConexionJavaMongo {
+    public static void main(String args []){
+        //Conectando con el servidor
+        MongoClient mongoClient = new MongoClient("localhost" , 27017);
+        System.out.println("Server Conectado Correctamente");
+        
+        //Conectar con la base de datos de Mongo
+        //MongoDatabase DBS = mongoClient.getDatabase("")
+        MongoDatabase dbs = mongoClient.getDatabase("test");
+	System.out.println("Connect to database successfully");
+	System.out.println("Database Name"+dbs.getName());
+        
+        /*List<String> bdNombres = mongoClient.getDatabaseNames();
+        System.out.println(bdNombres);*/
+        
+    }
     
 }
