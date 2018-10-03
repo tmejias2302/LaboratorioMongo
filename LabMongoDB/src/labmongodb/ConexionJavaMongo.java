c601725965279f380ec0515dc08816c09633d8c7
Thomas Mejias <tmejias2302@gmail.com>
2018-10-03 12:36:00 -0600

    Initial commit.

--- LabMongoDB/src/labmongodb/ConexionJavaMongo.java
+++ LabMongoDB/src/labmongodb/ConexionJavaMongo.java
@@ -1,25 +1,70 @@
 package labmongodb;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import static com.mongodb.client.model.Filters.eq;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
-
+import java.util.Map;
+import org.bson.Document;
 
 public class ConexionJavaMongo {
-    public static void main(String args []){
+
+    public static void main(String args[]) {
         //Conectando con el servidor
-        MongoClient mongoClient = new MongoClient("localhost" , 27017);
-        System.out.println("Server Conectado Correctamente");
+        MongoClient mongoClient = new MongoClient("localhost", 27017);
+
+        //Creando la base de datos
+        MongoDatabase BD = mongoClient.getDatabase("Setimo_Arte");
+
+        // Retrieving a collection
+        MongoCollection<Document> Coleccion_Pelicula = BD.getCollection("Pelicula");
+
+        //Creando la coleccion para la base de datos
+        //DBCollection Coleccion_Productora = (DBCollection) BD.getCollection("Compañia_Productora");
         
-        //Conectar con la base de datos de Mongo
-        //MongoDatabase DBS = mongoClient.getDatabase("")
-        MongoDatabase dbs = mongoClient.getDatabase("test");
-	System.out.println("Connect to database successfully");
-	System.out.println("Database Name"+dbs.getName());
+        //Agregar informacion a al documentoPelicula
+        List<String> actores = Arrays.asList("Thomas", "Luis");
+        Document documentoPelicula = new Document("Nombre_Pelicula", "Rango")
+                .append("Genero", "Comedia").append("Nombre_Director", "Jaime")
+                .append("Franquicia", "Faketon").append("Pais_Produccion", "USA")
+                .append("Año_Estreno", 2011).append("Duracion_Minutos", 165)
+                .append("Compañia_Productora", "Los pollos hermanos")
+                .append("Actores", actores);
+        Coleccion_Pelicula.insertOne(documentoPelicula);
         
-        /*List<String> bdNombres = mongoClient.getDatabaseNames();
-        System.out.println(bdNombres);*/
+        //Borrar valores especificos de un documento
+        Coleccion_Pelicula.deleteOne(eq("Nombre_Pelicula", "Rango"));
+        Coleccion_Pelicula.deleteOne(eq("Nombre_Pelicula", "Rango"));
+        Coleccion_Pelicula.deleteOne(eq("Nombre_Pelicula", "Rango"));
         
+        try (MongoCursor<Document> cur = Coleccion_Pelicula.find().iterator()) {
+            while (cur.hasNext()) {
+
+                Document doc = cur.next();
+
+                List list = new ArrayList(doc.values());
+                System.out.print(list.get(1)+ "\n");
+                System.out.print(list.get(2)+ "\n");
+                System.out.print(list.get(3)+ "\n");
+                System.out.print(list.get(4)+ "\n");
+                System.out.print(list.get(5)+ "\n");
+                System.out.print(list.get(6)+ "\n");
+                System.out.print(list.get(7)+ "\n");
+                System.out.print(list.get(8)+ "\n");
+                System.out.print(list.get(9)+ "\n");
+            }
+        }
+
+        mongoClient.close();
+
     }
-    
+
 }
